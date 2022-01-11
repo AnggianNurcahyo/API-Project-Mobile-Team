d36726e8f8ad669f2d7559ff757bcb9f03fb2111
AnggiPrasetyo30 <anggiprasetyo2000@gmail.com>
2022-01-11 07:07:13 +0700

    upload layout

---
+++ app/src/main/java/com/darryncampbell/dwgettingstartedjava/select_scanner_leader_locked.java
@@ -0,0 +1,14 @@
+package com.darryncampbell.dwgettingstartedjava;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class select_scanner_leader extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_scanner_leader);
+    }
+}
